96b65d19f3ce507566d463fa1889bd132609024e
alexandrahotti <alexandrahotti@hotmail.se>
2019-09-01 17:46:30 +0200

    Added solution TTT3d

--- Assignment2 - Zero Sum Games/3D_TicTacToe/Player.java
+++ Assignment2 - Zero Sum Games/3D_TicTacToe/Player.java
@@ -1,10 +1,8 @@
-//****3D3D3D******#************###################************####*#
-//hjhebdhehjde
 import java.util.*;
 
 public class Player {
 
-    //double bestv=0;
+
     /**
      * Performs a move
      *
@@ -14,12 +12,14 @@ public class Player {
      *            time before which we must have returned
      * @return the next state the board is in after our move
      */
+
      int[][] indicies =
-     {{0,1,2,3},//therowsumnsinthelayers
-     {4,5,6,7},
-     {8,9,10,11},
-     {12,13,14,15},
-     {16,17,18,19},
+     {
+     { 0,1,2,3},
+     { 4,5,6,7},
+     { 8,9,10,11},
+     { 12,13,14,15},
+     { 16,17,18,19},
      {20,21,22,23},
      {24,25,26,27},
      {28,29,30,31},
@@ -30,8 +30,8 @@ public class Player {
      {48,49,50,51},
      {52,53,54,55},
      {56,57,58,59},
-     {60,61,62,63},//therowsumnsinthelayers
-     {0,4,8,12},//thecolumnsinthelayers
+     {60,61,62,63},
+     {0,4,8,12},
      {1,5,9,13},
      {2,6,10,14},
      {3,7,11,15},
@@ -46,16 +46,16 @@ public class Player {
      {48,52,56,60},
      {49,53,57,61},
      {50,54,58,62},
-     {51,55,59,63},//thecolumnsinthelayers
-     {0,5,10,15},//layer1diagonals
-     {3,6,9,12},//layer1diagonals
-     {16,21,26,31},//layer2diagonals
-     {19,22,25,28},//layer2diagonals
-     {32,37,42,47},//layer3diagonals
-     {35,38,41,44},//layer3diagonals
-     {48,53,58,63},//layer4diagonals
-     {51,54,57,60},//layer4diagonals
-     {0,16,32,48},//vertikal"columns"
+     {51,55,59,63},
+     {0,5,10,15},
+     {3,6,9,12},
+     {16,21,26,31},
+     {19,22,25,28},
+     {32,37,42,47},
+     {35,38,41,44},
+     {48,53,58,63},
+     {51,54,57,60},
+     {0,16,32,48},
      {1,17,33,49},
      {2,18,34,50},
      {3,19,35,51},
@@ -70,15 +70,15 @@ public class Player {
      {12,28,44,60},
      {13,29,45,61},
      {14,30,46,62},
-     {15,31,47,63},//vertikal"columns"
-     {0,17,34,51},//diagonalsvertically
+     {15,31,47,63},
+     {0,17,34,51},
      {4,21,38,55},
      {8,25,42,59},
      {12,29,46,63},
      {3,18,33,48},
      {7,22,37,52},
      {11,26,41,56},
-     {15,30,45,60},//diagonalsvertically
+     {15,30,45,60},
      {0,20,40,60},
      {1,21,41,61},
      {2,22,42,62},
@@ -87,10 +87,10 @@ public class Player {
      {13,25,37,49},
      {14,26,38,50},
      {15,27,39,51},
-     {0,21,42,63},//diagonalsthrouughthecenter
+     {0,21,42,63},
      {3,22,41,60},
      {15,26,37,48},
-     {12,25,38,51}};//diagonalsthrouughthecenter
+     {12,25,38,51}};
 
 
      int [][] pointsPerPlayer = {
